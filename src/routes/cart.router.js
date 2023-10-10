792cbcd9a464ab615ad294a8c8c43c6389344d7f
DiegoSpehrs <diegospehrs@gmail.com>
2023-10-09 22:49:49 -0300

    7moDesafioEntregableDiegoSpehrs

--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -6,13 +6,13 @@ const router = Router();
 
 router.post('/', cartsController.createcart);
 
-router.post('/:cid/products/:pid', cartsController.addProduct);
+router.post('/:cid/products/:pid', cartsController.addProduct);//no funca
 
 router.delete('/:cid/products/:pid', cartsController.productDelete);
 
-router.put('/:cid', cartsController.getCart);
+router.put('/:cid', cartsController.getCart);//no funca
 
-router.put('/:cid/products/:pid', cartsController.updateProduct);
+router.put('/:cid/products/:pid', cartsController.updateProduct);//no funca
 
 router.delete('/:cid', cartsController.cartDelete);
 
